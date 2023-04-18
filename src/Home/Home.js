a1c248be4fabd54b91b993943f5453299c26555a
kirangregerson <kirangregerson@gmail.com>
2023-04-17 20:03:05 -0400

    Added Searchbar Styling

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,29 +7,31 @@ import NavigationSidebar from "../Navigation-Sidebar/Navigation-Sidebar";
 
 const Home = () => {
   return (
-      <div className="row">
-          <NavigationSidebar active={"home"}/>
-          <div className="col-8">
-              <h1>Welcome home!</h1>
-              {/* TODO: If logged in:*/}
-              <ItemCarousel/>
-              <section>
-                  <h2>You've recently liked:</h2>
-              </section>
-              <section>
-                  <h2> (Person who's stuff you've liked) also has these for sale: </h2>
-              </section>
-              {/*TODO: If not logged in */}
-              <section>
-                  <h2> Other people have recently liked: </h2>
-              </section>
-              <section>
-                  <h2>Ad about signing up to get a sale or something</h2>
-              </section>
-              {/*TODO: Generic content*/}
-              <section>
-                  <h2>Popular accounts:</h2>
-              </section>
+      <div className="container">
+          <div className="row">
+              <NavigationSidebar active={"home"}/>
+              <div className="col-8">
+                  <h1>Welcome home!</h1>
+                  {/* TODO: If logged in:*/}
+                  <ItemCarousel/>
+                  <section>
+                      <h2>You've recently liked:</h2>
+                  </section>
+                  <section>
+                      <h2> (Person who's stuff you've liked) also has these for sale: </h2>
+                  </section>
+                  {/*TODO: If not logged in */}
+                  <section>
+                      <h2> Other people have recently liked: </h2>
+                  </section>
+                  <section>
+                      <h2>Ad about signing up to get a sale or something</h2>
+                  </section>
+                  {/*TODO: Generic content*/}
+                  <section>
+                      <h2>Popular accounts:</h2>
+                  </section>
+              </div>
           </div>
       </div>
   );
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,7 +1,11 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import {
+    faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 import { getProducts } from "../Services/StoreService";
 import SearchItem from "./SearchItem";
 import NavigationSidebar from "../Navigation-Sidebar/Navigation-Sidebar";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Search = () => {
   const [items, setItems] = useState([]);
@@ -17,10 +21,24 @@ const Search = () => {
         <div className="row">
             <NavigationSidebar active="search"/>
             <div className="col-8">
-                <div>
-                    <input className=""></input>
-                    <button>Search</button>
+                <div className="input-group">
+                    <input className="form-control py-2 rounded-pill mr-1 pr-5" type="search" placeholder="Search"></input>
+                    <span className="input-group-append">
+                        <button className="btn rounded-pill border-0 ml-n5" type="button">
+                            <FontAwesomeIcon icon={faSearch} className="position-absolute wd-nudge-up"/>
+                        </button>
+                    </span>
                 </div>
+                {/*<div className="row">*/}
+                {/*    <div className="col-1">*/}
+                {/*        <FontAwesomeIcon icon={faSearch} className="position-absolute*/}
+                {/*       wd-nudge-up"/>*/}
+                {/*    </div>*/}
+                {/*    <div className="col-4">*/}
+                {/*        <input placeholder="Search Linens"*/}
+                {/*               className="form-control rounded-pill ps-5"/>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
                 <ul className="list-group">
                     {items.map((item) => {
                         return <SearchItem item={item}></SearchItem>;
