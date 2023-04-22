ec064e6ba4936cefa32aa14ea138a75041a4d3b1
kirangregerson <kirangregerson@gmail.com>
2023-04-22 17:40:40 -0400

    Got functioning frontend login

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Login from "./Login";
+import Login from "./Login/Login";
 import Search from "./Search/Search";
 import Home from "./Home/Home";
 import Details from "./Details/Details";
 import NavigationSidebar from "./Navigation-Sidebar/Navigation-Sidebar";
+import Profile from "./Profile/Profile";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  //JSON stringify is needed to serialize value before storing. This is reversed at use time.
+  localStorage.setItem("loggedIn", JSON.stringify(loggedIn));
 
   return (
     <BrowserRouter>
@@ -25,6 +28,7 @@ function App() {
           ></Route>
           <Route path="/search" element={<Search />}></Route>
           <Route path="/item/:productId" element={<Details />}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
