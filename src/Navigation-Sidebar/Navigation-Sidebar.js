6ab76e09c056bc107cfebb45dcb5195505cc0081
ztaggart <ztaggart020701@gmail.com>
2023-04-22 22:22:18 -0400

    fixes

--- src/Navigation-Sidebar/Navigation-Sidebar.js
+++ src/Navigation-Sidebar/Navigation-Sidebar.js
@@ -1,60 +1,80 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faBell,
-    faEllipsisH,
-    faHome,
-    faSearch,
-    faUser
+  faBell,
+  faEllipsisH,
+  faHome,
+  faSearch,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 
 // import "./Navigation-Sidebar.css";
 
-const NavigationSidebar = (
-    {
-        active = 'home',
-    }
-) => {
-    const loggedInStr = localStorage.getItem("loggedIn")
-    const loggedIn = JSON.parse(loggedInStr);
-    return (
-        <div className="list-group col-2">
-            <a className={`list-group-item
-                ${active === 'home'?'active':''}`}
-               href="/home">
-                <FontAwesomeIcon icon={faHome} />
-                &nbsp;
-                Home
-            </a>
-            <a className={`list-group-item
-                ${active === 'search'?'active':''}`}
-               href="/search">
-                <FontAwesomeIcon icon={faSearch} />
-                &nbsp;
-                Search
-            </a>
-            <a className={`list-group-item
-                ${active === 'notifications'?'active':''}`}
-               href="/notifications">
-                <FontAwesomeIcon icon={faBell} />
-                &nbsp;
-                Notifications
-            </a>
-            <a className={`list-group-item
-                ${active === 'profile'?'active':''}`}
-               href={loggedIn? ("/profile") : ("/login")}>
-                <FontAwesomeIcon icon={faUser} />
-                &nbsp;
-                Profile
-            </a>
-            <a className={`list-group-item
-                ${active === 'more'?'active':''}`}
-               href="/more">
-                <FontAwesomeIcon icon={faEllipsisH} />
-                &nbsp;
-                More
-            </a>
-        </div>
-    );
+const NavigationSidebar = ({ active = "home" }) => {
+  const loggedInStr = localStorage.getItem("loggedIn");
+  const loggedIn = JSON.parse(loggedInStr);
+
+  async function logOut() {
+    localStorage.clear();
+    axios.post(`${process.env.REACT_APP_API_BASE}/api/logout`);
+  }
+
+  return (
+    <div className="list-group col-2">
+      <a
+        className={`list-group-item
+                ${active === "home" ? "active" : ""}`}
+        href="/home"
+      >
+        <FontAwesomeIcon icon={faHome} />
+        &nbsp; Home
+      </a>
+      <a
+        className={`list-group-item
+                ${active === "search" ? "active" : ""}`}
+        href="/search"
+      >
+        <FontAwesomeIcon icon={faSearch} />
+        &nbsp; Search
+      </a>
+      <a
+        className={`list-group-item
+                ${active === "notifications" ? "active" : ""}`}
+        href="/notifications"
+      >
+        <FontAwesomeIcon icon={faBell} />
+        &nbsp; Notifications
+      </a>
+      <a
+        className={`list-group-item
+                ${active === "profile" ? "active" : ""}`}
+        href={loggedIn ? "/profile" : "/login"}
+      >
+        <FontAwesomeIcon icon={faUser} />
+        &nbsp;
+        {loggedIn ? "Profile" : "Log in"}
+      </a>
+      {loggedIn && (
+        <a
+          className={`list-group-item
+                ${active === "profile" ? "active" : ""}`}
+          onClick={logOut}
+          href="/"
+        >
+          <FontAwesomeIcon icon={faUser} />
+          &nbsp; Log out
+        </a>
+      )}
+      <a
+        className={`list-group-item
+                ${active === "more" ? "active" : ""}`}
+        href="/more"
+      >
+        <FontAwesomeIcon icon={faEllipsisH} />
+        &nbsp; More
+      </a>
+    </div>
+  );
 };
 export default NavigationSidebar;
